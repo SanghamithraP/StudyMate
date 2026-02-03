632cdc7f387f585bd618ed08c1cd280f3522883c
SanghamithraP <sanghamithra.p2024aiml@sece.ac.in>
2026-02-03 15:58:27 +0530

    Add files via upload

---
+++ pdf_parser.py
@@ -0,0 +1,16 @@
+import pymupdf as fitz
+
+def extract_text_from_pdf(uploaded_file):
+    doc = fitz.open(stream=uploaded_file.read(), filetype="pdf")
+    text = ""
+    for page in doc:
+        text += page.get_text("text") + "\n"
+    return text
+
+def chunk_text(text, chunk_size=500, overlap=100):
+    words = text.split()
+    chunks = []
+    for i in range(0, len(words), chunk_size - overlap):
+        chunk = " ".join(words[i:i+chunk_size])
+        chunks.append(chunk)
+    return chunks
